758db01c7b9b0913a5c2dab85a1c0dd1847795b9
0x0FACED <https.genius@gmail.com>
2025-05-03 15:54:52 +0300

    chore(idx-table): added Encode and Decode methods that doesnt work correctly

--- pkg/core/v1/types/index_table.go
+++ pkg/core/v1/types/index_table.go
@@ -1,5 +1,41 @@
 package types
 
+import (
+	"encoding/binary"
+	"io"
+)
+
 type IndexTable struct {
 	Secrets []SecretMeta // slice of secrets metadata
+}
+
+// TEST METHOD
+func (t *IndexTable) Encode(w io.Writer) error {
+	count := uint32(len(t.Secrets))
+	if err := binary.Write(w, binary.LittleEndian, count); err != nil {
+		return err
+	}
+
+	for _, secret := range t.Secrets {
+		if err := binary.Write(w, binary.LittleEndian, secret); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// TEST METHOD
+func (t *IndexTable) Decode(r io.Reader) error {
+	var count uint32
+	if err := binary.Read(r, binary.LittleEndian, &count); err != nil {
+		return err
+	}
+
+	t.Secrets = make([]SecretMeta, count)
+	for i := uint32(0); i < count; i++ {
+		if err := binary.Read(r, binary.LittleEndian, &t.Secrets[i]); err != nil {
+			return err
+		}
+	}
+	return nil
 }

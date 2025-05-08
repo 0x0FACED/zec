90dd53534f7f14be4504596cad026db2526a4e31
0x0FACED <https.genius@gmail.com>
2025-05-08 18:00:30 +0300

    chore(secret-data): added func that writes Val []byte to file

--- pkg/core/v1/types/secret_data.go
+++ pkg/core/v1/types/secret_data.go
@@ -1,6 +1,29 @@
 package types
 
+import (
+	"fmt"
+	"os"
+)
+
 type SecretData struct {
 	Meta SecretMeta // not written as secret
 	Val  []byte     // secret data (must be excrypted + zipped, but not now)
+}
+
+func (sd *SecretData) WriteToFile(path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	n, err := f.Write(sd.Val)
+	if err != nil {
+		return err
+	}
+
+	if n != len(sd.Val) {
+		return fmt.Errorf("zec/types/secret_data.go: written file size is not equal to secret size")
+	}
+
+	return nil
 }

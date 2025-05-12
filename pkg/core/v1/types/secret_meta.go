7ac27da320acef3d2ab2ffce53d82035d275f1d1
0x0FACED <https.genius@gmail.com>
2025-05-12 17:22:11 +0300

    refactor: updated secret meta structure (increased nonce size to 24 bytes, added encrypt mode field)

--- pkg/core/v1/types/secret_meta.go
+++ pkg/core/v1/types/secret_meta.go
@@ -7,18 +7,25 @@ const (
 	File      = 0x02
 )
 
+const (
+	AEAD      = 0x01
+	Streaming = 0x02
+)
+
 // fixed size secret metadata structure
-// 80 bytes
+// 94 bytes
 type SecretMeta struct {
 	// UUID is not used. flag --name is required
-	Name       [32]byte // 32 bytes, secret ID (name or uuid). if name not provided - use uuid (not good)
-	Offset     uint64   // 8 bytes, offset of the secret in the file
-	Size       uint64   // 8 bytes, size of the secret
-	CreatedAt  uint64   // 8 bytes, time of creation (unix)
-	ModifiedAt uint64   // 8 bytes, time of last modification (unix)
-	Type       uint8    // 1 byte, secret type (0x01 — file, 0x02 — text, 0x03 — binary for example)
-	Flags      uint8    // 1 byte, bit flags (0x01 — encrypted, 0x02 — compressed, 0x04 — deleted for example)
-	Nonce      [12]byte // 12 bytes, iv for encryption (chacha20 or aes-gcm)
+	Name        [32]byte // 32 bytes, secret ID (name or uuid). if name not provided - use uuid (not good)
+	Offset      uint64   // 8 bytes, offset of the secret in the file
+	Size        uint64   // 8 bytes, size of the secret
+	CreatedAt   uint64   // 8 bytes, time of creation (unix)
+	ModifiedAt  uint64   // 8 bytes, time of last modification (unix)
+	Type        uint8    // 1 byte, secret type (0x01 — file, 0x02 — text, 0x03 — binary for example)
+	Flags       uint8    // 1 byte, bit flags (0x01 — encrypted, 0x02 — compressed, 0x04 — deleted for example)
+	_           [1]byte  // padding
+	Nonce       [24]byte // 24 bytes, iv for encryption (chacha20[:12] xchacha20[:],  or aes-gcm[:12])
+	EncryptMode uint8    // 1 byte, AEAD or Streaming chacha20
 }
 
 // NewSecretMeta used for plain text
@@ -36,7 +43,7 @@ func NewSecretMeta(id string, size uint64) (SecretMeta, error) {
 		ModifiedAt: 0,
 		Type:       0x01,
 		Flags:      0x01,
-		Nonce:      [12]byte{},
+		Nonce:      [24]byte{},
 	}
 
 	return meta, nil
@@ -57,7 +64,7 @@ func NewSecretMetaWithType(id string, size uint64, _type uint8) (SecretMeta, err
 		ModifiedAt: 0,
 		Type:       _type,
 		Flags:      0x01,
-		Nonce:      [12]byte{},
+		Nonce:      [24]byte{},
 	}
 
 	return meta, nil
@@ -69,6 +76,17 @@ func (sm SecretMeta) TypeString() string {
 		return "Text"
 	case File:
 		return "File"
+	default:
+		return "Unknown"
+	}
+}
+
+func (sm SecretMeta) EncryptModeString() string {
+	switch sm.EncryptMode {
+	case AEAD:
+		return "AEAD"
+	case Streaming:
+		return "Streaming"
 	default:
 		return "Unknown"
 	}
